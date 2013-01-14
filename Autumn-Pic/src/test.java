20e1864f3089ec14946cfa023070addcbebf1d47
wangfei <littlewangfei@gmail.com>
2013-01-14 13:03:45 +0800

    test

---
+++ Autumn-Pic/src/test.java
@@ -0,0 +1,16 @@
+/*
+ * @(#)test.java 2013-1-14 обнГ01:03:33
+ * Autumn-Pic
+ * Copyright 2013 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * test
+ * @author wfei
+ * @version 1.0
+ *
+ */
+public class test {
+
+}
